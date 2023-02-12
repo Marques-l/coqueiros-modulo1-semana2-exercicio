7cae4d1449a3e0d42d66e4e4b71cd186c81fb5f5
Marques-l <125034468+Marques-l@users.noreply.github.com>
2023-02-12 05:40:51 -0300

    Add files via upload

---
+++ [M1S02] Ex 2/SLN-[M1S02] Ex 2/FichaInscricao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLN__M1S02__Ex_2
+{
+    public class FichaInscricao
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool MenorIdade { get; set; }
+        public FichaInscricao(int id, string nome, DateTime dataNascimento, bool menorIdade)
+        {
+            Id = id;
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            MenorIdade = menorIdade;
+        }
+        public void FichaIncricao () {
+           Console.WriteLine($"Código {Id}, Nome {Nome}, Data de nascimento {DataNascimento}, maior de idade: {MenorIdade}"); 
+
+            }
+        
+   }
+
+}
