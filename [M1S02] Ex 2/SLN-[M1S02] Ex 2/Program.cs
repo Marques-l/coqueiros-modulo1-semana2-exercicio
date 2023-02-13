fbad9391ca479a571c480d80bd4462d5d58cad16
Marques-l <125034468+Marques-l@users.noreply.github.com>
2023-02-13 09:56:41 -0300

    Delete [M1S02] Ex 2 directory

--- [M1S02] Ex 2/SLN-[M1S02] Ex 2/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using SLN__M1S02__Ex_2; 
-
-Console.WriteLine("Hello, World!");
-FichaInscricao nomeVariavel = new FichaInscricao(18, "lucas", DateTime.Now, true);
