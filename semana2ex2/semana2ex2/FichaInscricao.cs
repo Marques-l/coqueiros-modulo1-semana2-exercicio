47984d61ceb12a9a57dc69bc1c99a968b89b15d2
Marques-l <125034468+Marques-l@users.noreply.github.com>
2023-02-13 09:59:00 -0300

    Add files via upload

---
+++ semana2ex2/semana2ex2/FichaInscricao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana2ex2
+
+{
+    public class Fichainscricao /// Criação da class 
+    { /// criação da propriedades públicas
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool MenorIdade { get; set; }
+
+        private decimal custoReal; 
+        /// Construtor com parâmetros
+        public Fichainscricao(int id, string nome, DateTime dataNascimento, bool menorIdade)
+        {
+            /// Preencher as variáveis com os parâmetros do construtor
+            Id = id;
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            MenorIdade = menorIdade;
+
+        }
+        // Novo método que vai preencher a variável custoReal com o valor do parâmetro recebido (custoRealNovo)
+        public void CalcularCustoReal(decimal custoRealNovo) { 
+        custoReal= custoRealNovo;
+             
+            Console.WriteLine($"Código {Id}, Nome {Nome}, Data de nascimento {DataNascimento} menor de idade: {MenorIdade}");
+        }
+
+    }
+} 
---
+++ semana2ex2/semana2ex2/Program.cs
@@ -0,0 +1,5 @@
+﻿using semana2ex2;
+
+Console.WriteLine("Hello, World!");
+Fichainscricao nomeVariavel = new Fichainscricao(18, "lucas", DateTime.Now, false);
+nomeVariavel.CalcularCustoReal(10);
