47010ce8307879f1e3e54db6c5dd95e73d206ed8
Marques-l <125034468+Marques-l@users.noreply.github.com>
2023-02-14 19:53:29 -0300

    Add files via upload

---
+++ M1S02ex7/M1S02ex7/Program.cs
@@ -0,0 +1,3 @@
+﻿
+M1S02ex7.ex7.Animacao();
+M1S02ex7.ex7.Animacao("Toy Story");
---
+++ M1S02ex7/M1S02ex7/ex7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1S02ex7
+{
+    public static class ex7
+    {
+        public static void Animacao()
+        {
+            Console.WriteLine("Animação Frozen"); 
+        }
+        public static void Animacao(string tipo)
+        {
+            Console.WriteLine($"Animação {tipo}");
+        }
+    }
+}
