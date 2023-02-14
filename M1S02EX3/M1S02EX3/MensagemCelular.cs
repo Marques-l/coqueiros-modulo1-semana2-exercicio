e2deae111811ded7e9c0f3c45b552a521f3e7a7d
Marques-l <125034468+Marques-l@users.noreply.github.com>
2023-02-14 09:05:44 -0300

    Add files via upload

---
+++ M1S02EX3/M1S02EX3/MensagemCelular.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1S02EX3
+{
+    public class MensagemCelular { 
+        public int Telefone { get; set; }
+        public string Mensagem { get; set; }
+        public MensagemCelular(int telefone, string mensagem)
+        {
+            Mensagem= mensagem;
+            Telefone= telefone;
+             
+        } 
+        private void EnviarMensagemaoTelefone ()
+        
+        {
+            
+            Console.WriteLine("Método executado pelo console");
+            Console.WriteLine($"Telefone: {Telefone}, mensagem: {Mensagem}"); 
+        }
+        public void Executar (int v)
+        {
+            
+            EnviarMensagemaoTelefone(); 
+            Console.WriteLine("Método privado executado na classe"); 
+        }
+
+     }
+   public class MinhaClasse {
+
+    }
+}
---
+++ M1S02EX3/M1S02EX3/Program.cs
@@ -0,0 +1,4 @@
+﻿using M1S02EX3;
+
+MensagemCelular entrada = new MensagemCelular(911, "Oi");
+entrada.Executar(911); 
